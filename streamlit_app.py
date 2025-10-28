624c2f5614a4a149e55ef700c482e1cb7f9c08c4
Caroline <carolines_t@outlook.com>
2025-10-28 15:20:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,9 @@ st.write('The risk prediction for your chosen values is:')
 
 with st.sidebar:
   st.header('Choose variables for prediction')
-  curvature  = st.slider('Road Curve',0,1)
+  curvature  = st.select_slider('Road Curvature',0,1)
+  speed_limit = st.slider('Speed Limit', 25,70)
+  lighting = st.selectbox('Lighting',('daylight','dim', 'night'))
+  weather = st.selectbox('Weather',('rainy','clear', 'foggy'))
+  road_signs_present = st.selectbox('Road Signs Present',('True','False'))
+                          
