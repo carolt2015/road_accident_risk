1afc1267b82be396b5fe0c71742ead880dc859ca
Caroline <carolines_t@outlook.com>
2025-10-28 14:46:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title('Road Accident Risk Prediction')
 
-st.write('Choose variables to predict the risk')
+st.write('Choose variables to predict the risk:')
+
+with st.sidebar:
+  st.header('Input features')
+  curvature  = st.slider('Road Curve',0,1)
