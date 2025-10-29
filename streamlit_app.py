5b13bbdfe8e1afc81758e2ce9bb3e4f81ed22536
Caroline <carolines_t@outlook.com>
2025-10-29 10:43:37 -0700

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ data  = { 'curvature':curvature,
          'road_signs_present':road_signs_present
         }
 
-input_data = pd.Dataframe(data, index=[0])
+input_data = pd.DataFrame(data, index=[0])
 
 # Adjust input format as per the model
 # Encode variables
