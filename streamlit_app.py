1907ec4b0ee0f4d5761508fe4ccf62ec027f6250
Caroline <carolines_t@outlook.com>
2025-10-29 12:56:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,11 +70,12 @@ st.write("input_shape",input_row.shape)
 #prediction = road_model.predict(input_row)
 #st.subheader(f"The chance of road accident is :{prediction[0] * 100}%")
 x = 0.35
-st.subheader(f"At the chosen conditions, the chance of road accident is : green[{x * 100}%]")
+st.subheader(f"At the chosen conditions, the chance of road accident is :")
+st.write("green[x * 100]%")
 if x >= 0.5:
-    st.subheader("red[Please drive safely!]")
+    st.write("red[Please drive safely!]")
 else:
-     st.subheader("Have a great trip!")
+     st.write("green[Have a great trip!]")
 
                                                           
                           
