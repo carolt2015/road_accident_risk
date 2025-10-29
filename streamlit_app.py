3de5c6ced5e78461db9efd3819001060b4f3274e
Caroline <carolines_t@outlook.com>
2025-10-29 09:55:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
-import pickle
+import pickle 
+from sklearn.preprocessing import MinMaxScaler
 
-<!-- load the trained model -->
+# load the trained model 
+with open('model.pkl', 'rb') as file:
+  road_model = pickle.load(file)  
 
 st.title('Road Accident Risk Prediction')
 st.write('The chance of of road accident is :')
@@ -13,4 +16,36 @@ with st.sidebar:
   lighting = st.selectbox('Lighting',('daylight','dim', 'night'))
   weather = st.selectbox('Weather',('rainy','clear', 'foggy'))
   road_signs_present = st.selectbox('Road Signs Present',('True','False'))
+
+data  = { 'curvature':curvature, 
+         'speed_limit':speed_limit,
+         'lighting':lighting,
+         'weather': weather, 
+         'road_signs_present':road_signs_present
+        }
+
+input_data = pd.Dataframe(data, index=[0])
+
+# Adjust input format as per the model
+# Encode variables
+encode = ['lighting','weather']
+df_road = pd.getdummies(input_data,prefix=encode)
+
+# Convert boolean variables to int
+df_road['road_signs_present'] = df_road['road_signs_present'].astype(int)
+
+# Transform variable
+df_road['speed_limit'] = np.log(df_road['speed_limit'])
+scaler = MinMaxScaler(feature_range=(0,1))
+df_road['speed_limit'] = scaler.fit_transform(df_road[['speed_limit']])
+
+# Input data for prediction
+input_row = df_road[:1]
+
+# for debug, remove after testing
+st.write(df_road[:1])
+         
+prediction = road_model.predict(input_row)
+st.write(f"The chance of of road accident is :{prediction[0]}")
+                                                          
                           
