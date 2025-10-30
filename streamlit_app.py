325a22ab9428138329bdfba72b3aaa8df704f337
Caroline <carolines_t@outlook.com>
2025-10-30 09:36:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ df = pd.DataFrame(data,index=[0])
 # Adjust input format as per the model
 # Encode variables
 #encode = ['lighting','weather']
-df[['lighting_daylight','lighting_dim','lighting_night','weather_rainy','weather_clear','weather_foggy']] = pd.DataFrame([[0],[0],[0],[0],[0],[0]])
+df[['lighting_daylight','lighting_dim','lighting_night','weather_rainy','weather_clear','weather_foggy']] = pd.DataFrame([[0],[0],[0],[0],[0],[0]],index=df.index)
     
 df_road = pd.get_dummies(df,columns=['lighting','weather']).astype(int)
 st.write("Shape of df_road after adding dummies",df_road.shape)
